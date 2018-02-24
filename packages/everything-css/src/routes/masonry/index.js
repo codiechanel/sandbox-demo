2492a76029f04a4bf3cf530372452d79c577bc8d
codiechanel <codiechanel@gmail.com>
2018-02-24 10:54:51 +0800

    cool

--- packages/everything-css/src/routes/masonry/index.js
+++ packages/everything-css/src/routes/masonry/index.js
@@ -1,66 +1,64 @@
-import React from "react";
-import Paper from "material-ui/Paper";
-import axios from 'axios'
-import "./styles.css";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import AppBar from 'material-ui/AppBar';
+import Tabs, { Tab } from 'material-ui/Tabs';
+import Typography from 'material-ui/Typography';
+import Demo1 from './Demo1/'
+import Demo2 from './Demo2/'
 
-class App extends React.Component {
-  
-  constructor(props) {
-    super(props)
-    let images = []
-    images.push('https://placeholdit.co//i/150x150?bg=222222')
-    images.push('https://placeholdit.co//i/150x100?bg=222222')
-    images.push('https://placeholdit.co//i/150x200?bg=222222')
-    images.push('https://placeholdit.co//i/150x250?bg=222222')
-    images.push('https://placeholdit.co//i/150x200?bg=333333')
-    images.push('https://placeholdit.co//i/150x150?bg=333333')
-    images.push('https://placeholdit.co//i/150x100?bg=333333')
-    
-    images.push('https://placeholdit.co//i/150x250?bg=333333')
-    images.push('https://placeholdit.co//i/150x250?bg=444444')
-    images.push('https://placeholdit.co//i/150x100?bg=444444')
-    images.push('https://placeholdit.co//i/150x200?bg=444444')
-    images.push('https://placeholdit.co//i/150x150?bg=444444')
-    this.state = {
-      items: images
-    }
-  }
-  componentDidMount() {
-    // this.getMockData()
-    
-  }
-  async getMockData() {
-    // const resp = await axios.get('https://randomuser.me/api/')
-    // const resp = await axios.get('http://faker.hook.io/?property=lorem.paragraphs')
-    const resp = await axios.get('https://reqres.in/api/users')
-    let items = resp.data.data
-    this.setState({items})
-    console.dir(resp.data.data)
+function TabContainer(props) {
+  return (
+    <Typography component="div" style={{ padding: 8 * 3 }}>
+      {props.children}
+    </Typography>
+  );
+}
 
-  }
+TabContainer.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    marginTop: theme.spacing.unit * 3,
+    backgroundColor: theme.palette.background.paper,
+  },
+});
+
+class SimpleTabs extends React.Component {
+  state = {
+    value: 0,
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
   render() {
-    
+    const { classes } = this.props;
+    const { value } = this.state;
 
-    let panel = null
-    if (this.state.items.length > 0 ) {
-      panel = this.state.items.map((x, i) => {
-        return <div key={i} className="masonry-layout__panel">
-            <div className="masonry-layout__panel-content">
-            <img src={x} alt="Smiley face"  />
-            </div>
-          </div>
-
-      })
-    }
     return (
-       <Paper className="container">
-        <div className="masonry-layout">
-        {panel}
-
-        </div>
-       </Paper> 
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Tabs value={value} onChange={this.handleChange}>
+            <Tab label="Demo One"/>
+            <Tab label="Demo Two" />
+            <Tab label="Item Three" href="#basic-tabs" />
+          </Tabs>
+        </AppBar>
+        {value === 0 && <TabContainer><Demo1 /></TabContainer>}
+        {value === 1 && <TabContainer><Demo2 /></TabContainer>}
+        {value === 2 && <TabContainer>Item Three</TabContainer>}
+      </div>
     );
   }
 }
 
-export default App;
+SimpleTabs.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(SimpleTabs);
